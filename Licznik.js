5f5e8317be1ef3b6838182f107ad2736dee9d364
Kobe Bryant <kobas394@gmail.com>
2018-04-19 18:58:26 +0200

    update2

---
+++ Licznik.js
@@ -0,0 +1,44 @@
+
+
+function dodaj()
+{
+	alert('dodaj2')
+}
+
+
+
+// i - numer gracza 1-5
+// j - numer surowca 1-6
+// k - 1 = stan; 2 = przyrost
+
+
+const gracz = 
+{
+	hajs: 20
+	hajs_p: 1
+	zelazo: 0
+	zelazo_p: 1
+	tytan: 0
+	tytan_p: 1
+	roslinnosc: 0
+	roslinnosc_p: 1
+	energia: 0
+	energia_p: 1
+	cieplo: 0
+	cieplo_p: 1
+	
+	dodajHajs = function()
+	{
+	alert('dodajHajs')
+	 this.hajs = this.hajs + wartosc
+	document.getElementById('r').innerHTML= this.hajs
+	}
+
+	przyrostTytan(wartosc) = function(wartosc)
+	
+	this.tytan_p = this.tytan_p + wartosc
+	document.getElementById('rp').innerHTML= this.tytan_p
+	}
+}
+
+gracz1 = gracz
